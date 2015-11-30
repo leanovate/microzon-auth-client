9b9f837945821d4056dc302d4aa54c3f0b5a698e
Untoldwind <landru@untoldwind.net>
2015-11-30 23:22:06 +0100

    First scenario

--- gattling/src/main/scala/de/leanovate/microzon/auth/gatling/AuthSimulation.scala
+++ gattling/src/main/scala/de/leanovate/microzon/auth/gatling/AuthSimulation.scala
@@ -6,38 +6,39 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class AuthSimulation extends Simulation {
-  println("Hurra")
   val httpConf = http
     .baseURL("http://localhost:8080")
 
   val scn = scenario("Login / get token / logout")
-    .exec(
-      http("login").post("/v1/tokens").body(StringBody("{}")).check(
-        status.is(201),
-        jsonPath("$.raw").ofType[String].saveAs("authorization")
-      )
-    )
-    .repeat(5) {
-      exec(
-        http("get token").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
-          status.is(200)
+      .repeat(100) {
+        exec(
+          http("login").post("/v1/tokens").body(StringBody("{}")).check(
+            status.is(201),
+            jsonPath("$.raw").ofType[String].saveAs("authorization")
+          )
         )
-      )
-    }
-    .exec(
-      http("logout").delete("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
-        status.is(204)
-      )
-    )
-    .pause(2.second)
-//    .exec(
-//      http("check token invalid").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
-//        status.is(401)
-//      )
-//    )
+          .repeat(5) {
+            exec(
+              http("get token").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+                status.is(200)
+              )
+            )
+          }
+          .exec(
+            http("logout").delete("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+              status.is(204)
+            )
+          )
+          .pause(2.second)
+          .exec(
+            http("check token invalid").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+              status.is(401)
+            )
+          )
+      }
 
 
   setUp(
-    scn.inject(atOnceUsers(2))
+    scn.inject(atOnceUsers(10), rampUsers(400).over(2.minutes))
   ).protocols(httpConf)
 }
