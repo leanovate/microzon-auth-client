37224ad3f9e56fea65144b5ee2c038a0c6dc9bde
Untoldwind <landru@untoldwind.net>
2015-11-30 22:35:31 +0100

    Add id to token

--- gattling/src/main/scala/de/leanovate/microzon/auth/gatling/AuthSimulation.scala
+++ gattling/src/main/scala/de/leanovate/microzon/auth/gatling/AuthSimulation.scala
@@ -3,17 +3,41 @@ package de.leanovate.microzon.auth.gatling
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.concurrent.duration._
+
 class AuthSimulation extends Simulation {
   println("Hurra")
   val httpConf = http
     .baseURL("http://localhost:8080")
 
-  val scn = scenario("Login")
+  val scn = scenario("Login / get token / logout")
     .exec(
-      http("login").post("/v1/tokens").body(StringBody("{}"))
+      http("login").post("/v1/tokens").body(StringBody("{}")).check(
+        status.is(201),
+        jsonPath("$.raw").ofType[String].saveAs("authorization")
+      )
     )
+    .repeat(5) {
+      exec(
+        http("get token").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+          status.is(200)
+        )
+      )
+    }
+    .exec(
+      http("logout").delete("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+        status.is(204)
+      )
+    )
+    .pause(2.second)
+//    .exec(
+//      http("check token invalid").get("/v1/tokens/myself").header("Authorization", "Bearer ${authorization}").check(
+//        status.is(401)
+//      )
+//    )
+
 
   setUp(
-    scn.inject(atOnceUsers(1))
+    scn.inject(atOnceUsers(2))
   ).protocols(httpConf)
 }
